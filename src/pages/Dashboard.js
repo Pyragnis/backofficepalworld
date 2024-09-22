f06701e916f15f278268ce6cfa07e75d6718d79a
Precious <precious.soriano@ynov.com>
2024-09-22 02:28:42 +0200

    add: dashboard count users, orders, categories, products

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../components/sidebar';
+import axios from 'axios';
 
 const Dashboard = () => {
+  const [totalOrders, setTotalOrders] = useState(0);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalCategories, setTotalCategories] = useState(0);
+  const [totalProducts, setTotalProducts] = useState(0);
+
+  const fetchCounts = async () => {
+    try {
+      // Récupérer le nombre de commandes
+      const ordersResponse = await axios.get(`http://localhost:${process.env.REACT_APP_PORT_BDD_API}/api/orders/count`);
+      setTotalOrders(ordersResponse.data.count);
+
+      // Récupérer le nombre d'utilisateurs
+      const usersResponse = await axios.get(`http://localhost:${process.env.REACT_APP_PORT_BDD_API}/api/users/count`);
+      setTotalUsers(usersResponse.data.count);
+
+      // Récupérer le nombre de catégories
+      const categoriesResponse = await axios.get(`http://localhost:${process.env.REACT_APP_PORT_BDD_API}/api/category/count`);
+      setTotalCategories(categoriesResponse.data.count);
+
+      // Récupérer le nombre de produtis
+      const productsResponse = await axios.get(`http://localhost:${process.env.REACT_APP_PORT_BDD_API}/api/products/count`);
+      setTotalProducts(productsResponse.data.count);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des statistiques:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCounts();
+  }, []);
+
   return (
     <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
       <Sidebar />
 
-      {/* Contenu principal */}
       <main className="flex-1 p-8">
         <section className="max-w-4xl mx-auto">
           <h1 className="text-4xl font-bold mb-6 text-gray-800">Bienvenue sur votre tableau de bord</h1>
@@ -22,6 +52,27 @@ const Dashboard = () => {
               <li>Consultez l'historique des commandes pour suivre vos ventes et améliorer votre service client.</li>
               <li>Utilisez la barre de navigation pour accéder rapidement aux différentes sections de l'application.</li>
             </ul>
+          </div>
+        </section>
+
+        <section className="max-w-4xl mx-auto mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
+            <div className="bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold text-gray-700">Total utilisateurs</h2>
+              <p className="text-3xl font-bold text-green-600">{totalUsers}</p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold text-gray-700">Total commandes</h2>
+              <p className="text-3xl font-bold text-sky-600">{totalOrders}</p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold text-gray-700">Total catégories</h2>
+              <p className="text-3xl font-bold text-yellow-600">{totalCategories}</p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold text-gray-700">Total produits</h2>
+              <p className="text-3xl font-bold text-red-600">{totalProducts}</p>
+            </div>
           </div>
         </section>
       </main>
